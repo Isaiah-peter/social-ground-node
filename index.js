fd12790bd7fd03f11dd0b7ac57bb89049e2a9f0a
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-09-29 15:11:07 +0100

    make it better

---
+++ index.js
@@ -0,0 +1,51 @@
+const port = process.env.PORT;
+
+const io = require("socket.io")(port, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  console.log(userId, socketId);
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  //take userid and socketid fromuser
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get messsge
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    console.log(user, "single user");
+    console.log(users, "sendMessage");
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("someone disconnect");
+    removeUser(socket.id);
+    io.emit("getUser", users);
+  });
+});
+
+console.log("welcom");
